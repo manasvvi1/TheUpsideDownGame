1c8e277d6a36e9164bf683a139883726cc8dfc43
Manasvvi <manasvvi.aggarwal@gmail.com>
2022-09-12 15:41:57 +0530

    added score text

--- TheUpsideDown/Assets/scripts/ScoringScript.cs
+++ TheUpsideDown/Assets/scripts/ScoringScript.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class ScoringScript : MonoBehaviour {
 
     public GameObject ScoreText;
-    public int theScore;
+    public int Weapons;
     public AudioSource collectSound;
 
     void OnTriggerEnter(Collider other) {
         collectSound.Play();
-        theScore += 1;
-        ScoreText.GetComponent<Text>().text = "Weapons Collected: " + theScore + "/5";
+        Weapons += 1;
+        ScoreText.GetComponent<Text>().text = "WEAPONS COLLECTED: " + Weapons + "/5";
         Destroy(gameObject);
     }
 }

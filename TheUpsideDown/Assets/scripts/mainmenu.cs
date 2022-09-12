dfec0b2fb418ceece5d38a789326715f19f21429
Manasvvi <manasvvi.aggarwal@gmail.com>
2022-09-12 21:45:52 +0530

    scene manager added, introductory page etc

---
+++ TheUpsideDown/Assets/scripts/mainmenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playgame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void quitgame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+
+    }
+}
